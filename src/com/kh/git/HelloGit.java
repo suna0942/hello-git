1c758501e1478730efc356c279ee4583e220e5aa
suna0942 <suna0942@naver.com>
2022-06-13 16:59:13 +0900

    버전1 만들기

--- src/com/kh/git/HelloGit.java
+++ src/com/kh/git/HelloGit.java
@@ -5,4 +5,12 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("HelloGit!!!");
 	}
+
+	public void test1() {
+		System.out.println("git verson 만들기..");
+	}
+
+	
+
+	
 }
