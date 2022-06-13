c192b724505103c62e0d5adae230ded8883983dd
suna0942 <suna0942@naver.com>
2022-06-13 17:38:40 +0900

    Dog 기능 완성!

--- src/com/kh/git/HelloGit.java
+++ src/com/kh/git/HelloGit.java
@@ -1,15 +1,16 @@
 package com.kh.git;
 
+import com.kh.git.animal.Dog;
+
 public class HelloGit {
 	
 	public static void main(String[] args) {
 		System.out.println("HelloGit!!!");
+		
+		// Dog 기능 추가
+		Dog dog = new Dog();
+		dog.bark();
 	}
-
-	public void test1() {
-		System.out.println("git verson 만들기..");
-	}
-	
 
 	
 }
