b502ce3822f178869d9e70f68d7f4202ef0a84b8
suna0942 <96761538+suna0942@users.noreply.github.com>
2022-06-13 20:36:34 +0900

    Snake#sound 메소드 추가

--- src/com/kh/git/animal/Snake.java
+++ src/com/kh/git/animal/Snake.java
@@ -2,7 +2,11 @@ package com.kh.git.animal;
 
 public class Snake {
   public void run () {
-  System.out.printIn("snake run");
+    System.out.println("snake run");
+  }
+  
+  public void sound() {
+    System.out.println("스으으으");
   }
   
 }
